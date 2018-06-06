3065f1ce48717a872cc1686a4d41458b2f2afd77
Kevin Piña <kevin@kevinpina.com>
2018-06-05 22:29:47 -0400

    Arreglando Codigo 2

---
+++ src/main/java/com/bancochile/controller/package-info.java
@@ -0,0 +1,4 @@
+/**
+	Classes that hold all the necessary for the Controllers.
+*/
+package com.bancochile.controller;
---
+++ src/main/java/com/bancochile/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+	Package that contains basic beans.
+*/
+package com.bancochile.domain;
